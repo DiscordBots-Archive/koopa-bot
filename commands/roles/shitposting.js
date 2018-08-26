eaee49fb755268444889bd9f0b6efd8e6b8c554b
Glitch (koopa-bot) <none>
2018-08-26 15:24:16 +0000

    📒🚏 Checkpoint ./commands/roles/shitposting.js:657824/859 ./watch.json:657824/6

--- commands/roles/shitposting.js
+++ commands/roles/shitposting.js
@@ -5,6 +5,7 @@ module.exports = class ShitpostingRoleCommand extends Command {
         super(client, {
             name: 'shitposting',
             group: 'roles',
+            aliases: ["shitpost", "sp"],
             memberName: 'shitposting',
             description: 'Enables shitposting access.',
             examples: ['shitposting'],
@@ -14,7 +15,16 @@ module.exports = class ShitpostingRoleCommand extends Command {
 
     run(message) {
       var config = {};
-        var shitpostRole = message.guild.roles.find('name', "Shitpost"); //Define the community role
-        
+        var shitpostRole = message.guild.roles.find('name', "Shitposting"); //Define the sp role
+        if (!shitpostRole) return message.reply("an error occurred while running the command: `Role \"Shitposting\" not found`\nYou shouldn't ever receive an error like this.\nPlease contact Samplasion#7901.");
+        var t = "";
+        if (message.member.roles.has(shitpostRole.id)) {
+          message.member.roles.remove(shitpostRole.id);
+          t = "removed from";
+        } else {
+          message.member.roles.add(shitpostRole);
+          t = "added to"
+        }
+      message.send(":ok_hand: Shitpost role "+t+" your roles.");
     }
 };
