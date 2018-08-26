6ef9098e0dec6fdfad928c0c6cb5a4dffef1a0f4
Glitch (koopa-bot) <none>
2018-08-26 17:57:43 +0000

    🍹🛣 Checkpoint ./commands/audio/play.js:657824/170

--- commands/audio/play.js
+++ commands/audio/play.js
@@ -22,10 +22,13 @@ module.exports = class PlayAudioCommand extends Command {
       if(!msg.member.voiceChannel) return msg.reply("you are not in a voice channel, *are you?* :eyes:");
       if(!this.client.audio.servers[msg.guild.id]) this.client.audio.servers[msg.guild.id] = {
         queue: [],
-        skips: 0
+        np: null,
+        skips: 0,
+        skippers: []
       }
       var server = this.client.audio.servers[msg.guild.id];
       server.queue.push(link);
+      msg.reply("your video has been added to the queue!");
       if(!msg.guild.voiceConnection) msg.member.voiceChannel.join().then(connection => {
         this.client.audio.play(connection, msg);
       });
