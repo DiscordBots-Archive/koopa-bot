5f3811bd7838d0f7ea3f0f7057d444414d9488ae
Glitch (koopa-bot) <none>
2018-08-26 09:37:38 +0000

    🗼⭐️ Checkpoint ./package.json:657824/4093 ./index.js:657824/1086 ./watch.json:657824/242

---
+++ index.js
@@ -0,0 +1,38 @@
+const { CommandoClient } = require("discord.js-commando");
+const path = require('path');
+
+// Stayin' alive
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
+const client = new CommandoClient({
+    commandPrefix: '!',
+//  unknownCommandResponse: false,
+    owner: '280399026749440000',
+    disableEveryone: true
+});
+
+client.registry
+    .registerDefaultTypes()
+    .registerGroups([
+        ['group1', 'Our First Command Group']
+    ])
+    .registerDefaultGroups()
+    .registerDefaultCommands()
+    .registerCommandsIn(path.join(__dirname, 'commands'));
+
+client.on('ready', () => {
+    console.log('Logged in!');
+    client.user.setActivity('game');
+});
+
+client.login('Your Secret Token');
