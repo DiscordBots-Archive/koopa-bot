de21a8cc85213d9fd2fb99718918f745c5a239c5
Glitch (koopa-bot) <none>
2018-08-26 10:50:05 +0000

    😂🍜 Checkpoint ./index.js:657824/1377 ./watch.json:657824/6

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { CommandoClient } = require("discord.js-commando");
+const { RichEmbed } = require("discord.js");
 const path = require('path');
 
 // Stayin' alive
@@ -14,6 +15,7 @@ setInterval(() => {
   http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
 }, 280000);
 
+// Set up the client
 const client = new CommandoClient({
     commandPrefix: '!',
     unknownCommandResponse: false,
@@ -32,8 +34,19 @@ client.registry
     .registerDefaultCommands()
     .registerCommandsIn(path.join(__dirname, 'commands'));
 
-client.on("guildMemberAdd", member => {
-    
+client.on('guildMemberAdd', member => {
+	var welcomechannel = member.guild.channels.find('name', 'general-talk');
+	if (!welcomechannel) return;
+
+	var embed = new RichEmbed()
+    .setThumbnail(member.guild.iconURL)
+		.setColor("#B30000")
+		.setTitle(`Welcome to Mario Modding, ${member.user.username}`)
+		.setDescription("Mario Modding is the board where you can talk about, well, Mario modding")
+		.addField("Website", "http://mario-modding.co.nf", true)
+		.setFooter(`Read #rules before starting`);
+
+	welcomechannel.send(embed);
 });
 
 client.on('ready', () => {
