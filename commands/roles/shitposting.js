2d4dbc8958425cdd00d147d0670ef28f33713aef
Glitch (koopa-bot) <none>
2018-08-26 10:29:34 +0000

    👐🏮 Checkpoint ./commands/roles/shitposting.js:657824/751

--- commands/roles/shitposting.js
+++ commands/roles/shitposting.js
@@ -12,7 +12,15 @@ module.exports = class ShitpostingRoleCommand extends Command {
         });
     }
 
-    run(msg) {
+    run(message) {
+        spoilersRole = message.guild.roles.find('name', config.SpoilersRole); //Define the spoiler role
+        var communityRole = message.guild.roles.find('name', config.CommunityRole), //Define the community role
+        modRole = message.guild.roles.find("name", config.ModRole), //Define that staff role
+        mutedRole = message.guild.roles.find('name', config.MutedRole), //Define the Muted role
+
+        logschannel = message.guild.channels.find("name", config.logschannel);
+        modlogschannel = message.guild.channels.find("name", config.modlogschannel);
+        modmailchannel = client.channels.get(config.modmailchannel);
         return msg.say('Hi, I\'m awake!');
     }
 };
