91d1f7a8a350f6bd2712cea9a64d9adceb937740
Glitch (koopa-bot) <none>
2018-08-26 11:21:30 +0000

    🍯🐢 Checkpoint ./index.js:657824/1677 ./watch.json:657824/6

--- index.js
+++ index.js
@@ -52,15 +52,13 @@ client.on('guildMemberAdd', member => {
 client.on("messageReactionAdd", async (reaction, user) => {
     const message = reaction.message;
      // This is the first check where we check to see if the reaction is not the unicode star emote.
-    if (reaction.emoji.name !== '⭐') return;
+    if (reaction.emoji.name !== '⭐') return message.send(/"not a");
     // This is our final check, checking to see if message was sent by a bot.
     if (message.author.bot) return message.channel.send(`${user}, you cannot star bot messages.`);
     // Here we get the starboard channel from the guilds settings. 
-    const starboardChannel = message.guild.channels.find("name", "starboard"); 
-    // Here we will find the channel
-    const starChannel = message.guild.channels.find(channel => channel.name == starboardChannel)
+    const starChannel = message.guild.channels.find("name", "starboard");
     // If there's no starboard channel, we stop the event from running any further, and tell them that they don't have a starboard channel.
-    if (!starChannel) return message.channel.send(`It appears that you do not have a \`${starboardChannel}\` channel.`); 
+    if (!starChannel) return message.channel.send(`It appears that you do not have a \`"starboard"\` channel.`); 
     // Here we fetch 100 messages from the starboard channel.
     const fetch = await starChannel.fetchMessages({ limit: 100 }); 
     // We check the messages within the fetch object to see if the message that was reacted to is already a message in the starboard,
@@ -88,7 +86,7 @@ client.on("messageReactionAdd", async (reaction, user) => {
     // Now we use an if statement for if a message isn't found in the starboard for the message.
     if (!stars) {
       // We use the this.extension function to see if there is anything attached to the message.
-      const image = message.attachments.size > 0 ? await this.extension(reaction, message.attachments.array()[0].url) : ''; 
+      const image = message.attachments.size > 0 ? await extension(reaction, message.attachments.array()[0].url) : ''; 
       // If the message is empty, we don't allow the user to star the message.
       if (image === '' && message.cleanContent.length < 1) return message.channel.send(`${user}, you cannot star an empty message.`); 
       const embed = new RichEmbed()
@@ -107,7 +105,31 @@ client.on("messageReactionAdd", async (reaction, user) => {
     }
 });
 
-client.on(messageReactionRemove", await (re)
+client.on("messageReactionRemove", async (reaction, user) => {
+    const message = reaction.message;
+    if (message.author.id === user.id) return;
+    if (reaction.emoji.name !== '⭐') return;
+    const { starboardChannel } = this.client.settings.get(message.guild.id);
+    const starChannel = message.guild.channels.find(channel => channel.name == starboardChannel)
+    if (!starChannel) return message.channel.send(`It appears that you do not have a \`${starboardChannel}\` channel.`); 
+    const fetchedMessages = await starChannel.fetchMessages({ limit: 100 });
+    const stars = fetchedMessages.find(m => m.embeds[0].footer.text.startsWith('⭐') && m.embeds[0].footer.text.endsWith(reaction.message.id));
+    if (stars) {
+      const star = /^\⭐\s([0-9]{1,3})\s\|\s([0-9]{17,20})/.exec(stars.embeds[0].footer.text);
+      const foundStar = stars.embeds[0];
+      const image = message.attachments.size > 0 ? await extension(reaction, message.attachments.array()[0].url) : '';
+      const embed = new RichEmbed()
+        .setColor(foundStar.color)
+        .setDescription(foundStar.description)
+        .setAuthor(message.author.tag, message.author.displayAvatarURL)
+        .setTimestamp()
+        .setFooter(`⭐ ${parseInt(star[1])-1} | ${message.id}`)
+        .setImage(image);
+      const starMsg = starChannel.fetchMessage(stars.id);
+    await starMsg.edit({ embed });
+    if(parseInt(star[1]) - 1 == 0) return starMsg.delete(1000);
+    }
+});
 
 function extension(reaction, attachment) {
   const imageLink = attachment.split('.');
