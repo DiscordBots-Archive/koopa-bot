6896c4aa423e20eee2d635e99eec034bca524110
Glitch (koopa-bot) <none>
2018-08-26 11:01:02 +0000

    🌼🚖 Checkpoint ./index.js:657824/1498

--- index.js
+++ index.js
@@ -49,6 +49,20 @@ client.on('guildMemberAdd', member => {
 	welcomechannel.send(embed);
 });
 
+client.on("messageReactionAdd", (reaction, user) => {
+    const message = reaction.message;
+     // This is the first check where we check to see if the reaction is not the unicode star emote.
+    if (reaction.emoji.name !== '⭐') return;
+    // This is our final check, checking to see if message was sent by a bot.
+    if (message.author.bot) return message.channel.send(`${user}, you cannot star bot messages.`);
+    // Here we get the starboard channel from the guilds settings. 
+    const { starboardChannel } = message.guild.channels.find("name", "starboard"); 
+    // Here we will find the channel
+    const starChannel = message.guild.channels.find(channel => channel.name == starboardChannel)
+    // If there's no starboard channel, we stop the event from running any further, and tell them that they don't have a starboard channel.
+    if (!starChannel) return message.channel.send(`It appears that you do not have a \`${starboardChannel}\` channel.`); 
+});
+
 client.on('ready', () => {
     console.log('Logged in!');
     client.user.setActivity('Mario Modding - YAMMS | http://mario-modding.co.nf');
