d2951c709255e608bcd9b8c35024bd8b48782d0d
Glitch (koopa-bot) <none>
2018-08-28 09:29:19 +0000

    🌟😼 Checkpoint ./index.js:657824/1002 ./package.json:657824/1834

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const YTDL = require("ytdl-core");
 var ytdl = YTDL;
 const Enmap = require("enmap");
 const Provider = require("enmap-sqlite");
+const SQLite = require("better-sqlite3");
+const sql = new SQLite('./scores.sqlite');
 
 //sqlite.open(path.join(__dirname, 'score.sqlite'));
 
@@ -158,6 +160,21 @@ function extension(reaction, attachment) {
 client.on('ready', () => {
     console.log('Logged in!');
     client.user.setActivity('Mario Modding - YAMMS | http://mario-modding.co.nf');
+  
+  // Check if the table "points" exists.
+  const table = sql.prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'scores';").get();
+  if (!table['count(*)']) {
+    // If the table isn't there, create it and setup the database correctly.
+    sql.prepare("CREATE TABLE scores (id TEXT PRIMARY KEY, user TEXT, guild TEXT, points INTEGER, level INTEGER);").run();
+    // Ensure that the "id" row is always unique and indexed.
+    sql.prepare("CREATE UNIQUE INDEX idx_scores_id ON scores (id);").run();
+    sql.pragma("synchronous = 1");
+    sql.pragma("journal_mode = wal");
+  }
+  
+  // And then we have two prepared statements to get and set the score data.
+  client.getScore = sql.prepare("SELECT * FROM scores WHERE user = ? AND guild = ?");
+  client.setScore = sql.prepare("INSERT OR REPLACE INTO scores (id, user, guild, points, level) VALUES (@id, @user, @guild, @points, @level);");
 });
 
 client.audio = {};
