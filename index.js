80f2a595f3490b815e4b17b3b09b63ce4d2329e2
Glitch (koopa-bot) <none>
2018-08-28 11:57:07 +0000

    🏀⛱ Checkpoint ./index.js:657824/1702

--- index.js
+++ index.js
@@ -93,7 +93,41 @@ client.on("message", message => {
   // not Discord.js-Commando.CommandoClient
   
   if (message.guild) {
+    // if the channel is shitposting, return (we don't want to
+    // let people level up by spamming in #shitposting)
     if (message.channel.name =="shitposting") return;
+    
+    // get score
+    let score = client.getScore.get(message.author.id, message.guild.id);
+    // if the user doesn't have a score, give him
+    if (!score) {
+      score = {
+        id: `${message.guild.id}-${message.author.id}`,
+        user: message.author.id,
+        guild: message.guild.id,
+        points: 0,
+        level: 1
+      }
+    }
+    // Increment the score
+    score.points++;
+
+    // Calculate the current level through MATH OMG HALP.
+    // 1 level is 50 messages
+    const curLevel = (score.points+50) / 50 // Math.floor(0.1 * Math.sqrt(score.points));
+
+    // Check if the user has leveled up, and let them know if they have:
+    if(score.level < curLevel) {
+      // Level up!
+      var embed = new RichEmbed()
+        .setAuthor(message.author.avatarURL, message.member.displayName)
+        .setTitle("Felicitations!")
+        .setDescription("*(sigh)*\n\nYou've leveled UP!")
+        .addField("New Level", curLevel)
+        .setFooter("Samplasion, why are you doing me this?");
+      // message.reply(`Felicitations *(sigh)*! You've leveled up to level **${curLevel}**!\nSamplasion, why are you doing me this?`);
+      message.channel.send(embed)
+    }
   }
 });
 
