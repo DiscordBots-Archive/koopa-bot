c992bd1ff18a36da4b94dd890eac8b6390d57fbd
Glitch (koopa-bot) <none>
2018-08-28 13:11:59 +0000

    👝✋ Checkpoint ./commands/level/clearbots.js:657824/1579 ./watch.json:657824/4 ./index.js:657824/20

--- commands/level/clearbots.js
+++ commands/level/clearbots.js
@@ -1,53 +1,26 @@
 const { Command } = require('discord.js-commando');
 const { RichEmbed } = require("discord.js");
 
-module.exports = class ScoreCommand extends Command {
+module.exports = class ClearBotsCommand extends Command {
     constructor(client) {
         super(client, {
-            name: 'points',
-            aliases: ["level"],
+            name: 'clear-bots',
+            aliases: ["c-b"],
             group: 'level',
-            memberName: 'points',
-            description: 'Know your way (to dem points)',
-            examples: ['points', 'level'],
-            args: [
-              {
-                key: "membre",
-                prompt: "who do you want me to stalk? :eyes:",
-                default: "",
-                type: "member"
-              }
-            ],
+            memberName: 'clear-bots',
+            description: 'Removes bots from the table',
+            examples: ['clear-bots'],
             guildOnly: true
         });
     }
 
-    async run(msg, { membre }) {
-      if (!membre) {
-        var member = await msg.guild.members.get(msg.author.id);
-      } else {
-        var member = membre;
-      }
-      const message = msg;
-      
-      let score = this.client.getScore.get(member.user.id, msg.guild.id);
-      if (!score) {
-        score = {
-          id: `${message.guild.id}-${message.author.id}`,
-          user: message.author.id,
-          guild: message.guild.id,
-          points: 0,
-          level: 1
+    async run(message) {
+      var nmsg = await message.say("Cleaning bots from table scores...");
+      message.guild.members.forEach(m => {
+        if (m.user.bot) {
+          this.client.cleanScore.run(m.user.id, m.guild.id);
         }
-      }
-      var embed = new RichEmbed()
-          .setAuthor(msg.member.displayName, msg.author.displayAvatarURL)
-          .setColor(member.highestRole.color)
-          .setTitle(membre ? membre.displayName + "'s stats" : "Your stats")
-          .addField("Points", score.points, true)
-          .addField("Level", score.level, true)
-          .setThumbnail(member.user.displayAvatarURL);
-      
-      msg.embed(embed);
+      });
+      nmsg.edit("Done!");
     }
 };
