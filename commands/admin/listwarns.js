63d91d20a09be5c1a65783acac0daa3f2c94827e
Glitch (koopa-bot) <none>
2018-08-28 17:51:18 +0000

    🎉💾 Checkpoint ./commands/admin/listwarns.js:657824/885 ./watch.json:657824/2

--- commands/admin/listwarns.js
+++ commands/admin/listwarns.js
@@ -1,4 +1,5 @@
 const { Command } = require('discord.js-commando');
+const { RichEmbed } = require("discord.js");
 
 module.exports = class ListWarningsCommand extends Command {
     constructor(client) {
@@ -23,7 +24,18 @@ module.exports = class ListWarningsCommand extends Command {
       if (!this.client.isOwner(msg.author)
           || !msg.member.roles.has("481492274333876224")
           || !msg.member.roles.has("481492388020486171")) return msg.reply("you don't have the permission to use this!");
-      this.client.warns.get.all(member.user.id);
+      const warns = this.client.warns.get.all(member.user.id);
+      // Now shake it and show it! (as a nice embed, too!)
+      const embed = new RichEmbed()
+        .setTitle("Warning List")
+        .setAuthor(this.client.user.username, this.client.user.avatarURL)
+        .setThumbnail(member.user.displayAvatarURL)
+        .setDescription("All the warnings for user " + member.displayName)
+        .setColor(15844367);
+
+      for(const data of warns) {
+        embed.addField(`Warning given by ${msg.guild.users.get(data.moderator).displayName}`, `Reason: \`${data.reason}\`\nDate: ${data.time}`);
+      }
       msg.say(":ok: User warned!")
     }
 };
