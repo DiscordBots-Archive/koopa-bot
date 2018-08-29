cb12357371ee36d215f81a72a5a69adeb70d92c5
Glitch (koopa-bot) <none>
2018-08-29 07:39:36 +0000

    🖖⛰ Checkpoint ./commands/server/quote.js:657824/1129

--- commands/server/quote.js
+++ commands/server/quote.js
@@ -5,13 +5,35 @@ module.exports = class ReplyCommand extends Command {
         super(client, {
             name: 'quote',
             group: 'group1',
-            memberName: 'reply',
-            description: 'Replies with a Message.',
-            examples: ['reply']
+            memberName: 'quote',
+            description: 'Adds a message to the server quotes.',
+            examples: ['quote -add <quote name> <message id>', "quote -del <quote name>", "quote -list"],
+            args: [
+              {
+                key: "flag",
+                prompt: "insert a valid flag. `-add -list -del`",
+                type: "flag",
+                default: ""
+              },
+              {
+                key: 'name',
+                prompt: 'insert a name gor the quote',
+                type: 'string'
+              },
+              {
+                key: 'message',
+                prompt: 'What text would you like the bot to say?',
+                type: 'message'
+              }
+            ]
         });
     }
 
-    run(msg) {
-        return msg.say('Hi, I\'m awake!');
+    run(msg, { flag, name, message }) {
+        switch (flag) {
+          case "add":
+            break;
+          
+        }
     }
 };
