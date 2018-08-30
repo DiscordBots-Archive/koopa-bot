d9575c103c8e543c3c63e8ff894d5967046a9fd7
Glitch (koopa-bot) <none>
2018-08-30 08:18:42 +0000

    🐀🎄 Checkpoint ./commands/admin/clearwarns.js:657824/121 ./watch.json:657824/4

--- commands/admin/clearwarns.js
+++ commands/admin/clearwarns.js
@@ -3,11 +3,12 @@ const { Command } = require('discord.js-commando');
 module.exports = class CleasWarningsCommand extends Command {
     constructor(client) {
         super(client, {
-            name: 'warn',
+            name: 'clear-warns',
+            aliases: ["c-w"],
             group: 'admin',
-            memberName: 'warn',
+            memberName: 'clear-warns',
             description: 'Warns an user',
-            examples: ['warn <user> <reason>'],
+            examples: ['warn <user>'],
             args: [
               {
                 key: "member",
@@ -23,6 +24,7 @@ module.exports = class CleasWarningsCommand extends Command {
           && !msg.member.roles.has("481492274333876224")
           && !msg.member.roles.has("481492388020486171")) return msg.reply("you don't have the permission to use this!");
       if (member.user.bot) return msg.reply("bots don't have to be warned, so don't have warnings!");
+      this.client.warns.delete.run(member.user.id, msg.guild.id);
       msg.say(":ok: User warnings cleared!");
     }
 };
