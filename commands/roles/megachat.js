998a16dd95958a01b7a7da3b88321fc0fd2c553a
Glitch (koopa-bot) <none>
2018-08-31 21:24:14 +0000

    🐮🍴 Checkpoint ./commands/roles/megachat.js:657824/1228

---
+++ commands/roles/megachat.js
@@ -0,0 +1,31 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class MegachatRoleCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'megachat',
+            group: 'roles',
+            aliases: ["shitpost", "sp"],
+            memberName: 'shitposting',
+            description: 'Enables shitposting access.',
+            examples: ['shitposting'],
+            clientPermissions: ['MANAGE_ROLES'],
+            guildonly: true
+        });
+    }
+
+    run(message) {
+      var config = {};
+        var shitpostRole = message.guild.roles.find('name', "Shitposting"); //Define the sp role
+        if (!shitpostRole) return message.reply("an error occurred while running the command: `Role \"Shitposting\" not found`\nYou shouldn't ever receive an error like this.\nPlease contact Samplasion#7901.");
+        var t = "";
+        if (message.member.roles.has(shitpostRole.id)) {
+          message.member.removeRole(shitpostRole);
+          t = "removed from";
+        } else {
+          message.member.addRole(shitpostRole);
+          t = "added to"
+        }
+      message.channel.send(":ok_hand: Shitpost role "+t+" your roles.");
+    }
+};
