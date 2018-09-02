d5c64fcb0ff45752f101d0a2d53c97de0fc926ff
Glitch (koopa-bot) <none>
2018-09-02 09:04:15 +0000

    🛩👨‍❤️‍💋‍👨 Checkpoint ./point-inhibitor.js:657824/148

--- point-inhibitor.js
+++ point-inhibitor.js
@@ -1,5 +1,11 @@
 exports.inhibite = (client, msg) => {
   var chn = msg.channel;
   var name = chn.name;
-  return (name.includes("meme") || name.includes("dank") || name.includes("shit") || name.includes("end-of-server") || name.includes("megachat"))
+  return (name.includes("meme")
+          || name.includes("dank")
+          || name.includes("shit")
+          || name.includes("end-of-server")
+          || name.includes("megachat")
+          || name.includes("music")
+          || name.includes("log"))
 }
