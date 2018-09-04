2ec9e78d1778ef9f29d6ab6c0120213708ab2355
Glitch (koopa-bot) <none>
2018-09-04 18:14:27 +0000

    💘✂️ Checkpoint ./commands/audio/play.js:657824/95 ./watch.json:657824/2

--- commands/audio/play.js
+++ commands/audio/play.js
@@ -60,7 +60,7 @@ module.exports = class PlayAudioCommand extends Command {
 
               let resp = '';
               for (var i in videos) {
-                  embed.addField(`**${parseInt(i)+1}.** ${videos[i].title}`, `Duration: \`[${videos[i].timestamp}]\``);
+                  embed.addField(`**${parseInt(i)+1}.** ${videos[i].title} \`[${videos[i].timestamp}]\``, `\u200b`);
               }
 
               embed.setDescription(`\n**Choose a number between** \`1-${videos.length}\` (in 30 seconds the command will be canceled)`);
