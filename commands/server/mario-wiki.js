f45cd8e6358742859e5e1ebbd46df139efe9c8de
Glitch (koopa-bot) <none>
2018-09-05 15:59:49 +0000

    🏮🐡 Checkpoint ./commands/server/mariowiki.js:657824/87 ./watch.json:657824/10 ./commands/server/mario-wiki.js:657824/202

--- commands/server/mario-wiki.js
+++ commands/server/mario-wiki.js
@@ -6,26 +6,28 @@ module.exports = class ReplyCommand extends Command {
     super(client, {
       name: 'mario-wiki',
       group: 'server',
+      aliases: ["m-w"],
       memberName: 'mario-wiki',
       description: 'Searches the Mario Wiki.',
       examples: ['mario-wiki Super Mario Bros.'],
       args: [
               {
-                  key: 'query',
-                  prompt: 'What text would you like the bot to search on the Mario Wiki?',
-                  type: 'string',
-                  validate: text => {
-                    if (text.length < 51) return true;
-                    return 'message content must be less than or equal to 50 characters.';
-                  }
-               }
-          ]
+                key: 'query',
+                prompt: 'What text would you like the bot to search on the Mario Wiki?',
+                type: 'string',
+                validate: text => {
+                  if (text.length < 51) return true;
+                  return 'message content must be less than or equal to 50 characters.';
+                }
+              }
+      ]
     });
   }
 
   async run(msg, { query }) {
     var desc = "Search Query for \""+query+'"\n\n';
-    var res = await get("https://www.mariowiki.com/w/api.php?action=opensearch&format=json&search="+encodeURI(query));
+    var { body: res } = await get("https://www.mariowiki.com/api.php?action=opensearch&format=json&search="+encodeURI(query));
+    console.log(res);
     var json = JSON.parse(res);
     for (var i = 0; i < json[1].length; i++) {
       desc += `[${json[1][i]}](${json[3][i]})`
@@ -34,5 +36,6 @@ module.exports = class ReplyCommand extends Command {
       .setTitle("Mario Wiki")
       .setDescription(desc)
       .setFooter("Powered by the Mario Wiki • https://www.mariowiki.com/")
+    msg.embed(embed);
   }
 };
