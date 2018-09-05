7db3f39467a8a455a68dbd31adb3f9d52be90878
Glitch (koopa-bot) <none>
2018-09-05 16:41:11 +0000

    👷🏣 Checkpoint ./commands/server/mario-wiki.js:657824/3813

--- commands/server/mario-wiki.js
+++ commands/server/mario-wiki.js
@@ -25,9 +25,10 @@ module.exports = class ReplyCommand extends Command {
     });
   }
 
-  async run(msg, { query }) {
+  async run(message, { query }) {
     var desc = "Search Query for \""+query+'"\n\n';
     var { body: json } = await get("https://www.mariowiki.com/api.php?action=opensearch&format=json&search="+encodeURI(query));
+    var len = json[1].length;
     for (var i = 0; i < json[1].length; i++) {
       desc += `• [${i+1}] [${json[1][i]}](${json[3][i]})\n`
     }
@@ -35,10 +36,44 @@ module.exports = class ReplyCommand extends Command {
       .setTitle("Mario Wiki")
       .setDescription(desc)
       .setFooter("Powered by the Mario Wiki • https://www.mariowiki.com/")
-      .setThumbnail("https://www.mariowiki.com/images/mariowiki.png")
-    var text = htmlToText.fromString('<h1>Hello World</h1>', {
-      wordwrap: 130
+      .setThumbnail("https://www.mariowiki.com/images/mariowiki.png");
+    message.embed(embed);
+    message.channel.awaitMessages(response => response.author.id == message.author.id
+                                            && !isNaN(response.content)
+                                            && parseInt(response.content) <= len
+                                            && parseInt(response.content) > 0, {
+      max: 1,
+      time: 30000,
+      errors: ['time'],
+    })
+    .then(async (collected) => {
+      var newq = json[1][parseInt(collected.first().content)];
+      var { body: newj } = await get("https://www.mariowiki.com/api.php?action=parse&format=json&page="+encodeURI(query));
+      var text = this.stripHTML(newj.parse.text)
+      const newe = this.client.util.embed()
+        .setTitle("Mario Wiki - " + newq)
+        .setDescription(text)
+        .setFooter("Powered by the Mario Wiki • https://www.mariowiki.com/")
+        .setThumbnail("https://www.mariowiki.com/images/mariowiki.png");
+      })
+      .catch(() => {
+        message.reply('command canceled!');
+      });
+  }
+  
+  stripHTML(text) {
+    text = text.replace(/<style([\s\S]*?)<\/style>/gi, '');
+    text = text.replace(/<script([\s\S]*?)<\/script>/gi, '');
+    text = text.replace(/<\/div>/ig, '\n');
+    text = text.replace(/<\/li>/ig, '\n');
+    text = text.replace(/<li>/ig, '  *  ');
+    text = text.replace(/<\/ul>/ig, '\n');
+    text = text.replace(/<\/p>/ig, '\n');
+    text = text.replace(/<br\s*[\/]?>/gi, "\n");
+    text = text.replace(/<[^>]+>/ig, '');
+    text = htmlToText.fromString(text, {
+      wordwrap: 1000
     });
-    msg.embed(embed);
+    return text;
   }
 };
