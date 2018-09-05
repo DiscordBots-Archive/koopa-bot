e5036e5b9f5f769f242cb51cc22f28a464a1e710
Glitch (koopa-bot) <none>
2018-09-05 08:53:27 +0000

    🎨👔 Checkpoint ./commands/classes/Command.js:657824/68 ./classes/Command.js:657824/10640

---
+++ classes/Command.js
@@ -0,0 +1,17 @@
+const Commando = require("discord.js-commando");
+
+class Command extends Commando.Command {
+  constructor(client, info) {
+		super(client, info);
+    
+    this.adminOnly = info.adminOnly || false;
+    this.modOnly = info.modOnly || false;
+	}
+  
+  hasPermission(msg, ownerOverride = true) {
+    if (this.adminOnly) {
+      return this.client.isOwner(msg.author) || msg.member.roles.has("481492274333876224") || msg.member.roles.has("481492388020486171")
+    } else if (this.modOnly) {
+    } else super(msg, ownerOverride);
+	}
+}
