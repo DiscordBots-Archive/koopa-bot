320faa4da2b6f212c1e1ff50215645abb1b92632
Glitch (koopa-bot) <none>
2018-09-06 19:09:18 +0000

    🗽🌲 Checkpoint ./commands/server/staff.js:657824/350 ./watch.json:657824/8

--- commands/server/staff.js
+++ commands/server/staff.js
@@ -18,12 +18,17 @@ module.exports = class ReplyCommand extends Command {
     var owners = await msg.guild.members.filter(m => m.roles.has(guildConf.ownerRole)).map(m => m.user.tag);
     var admins = await msg.guild.members.filter(m => m.roles.has(guildConf.adminRole)).map(m => m.user.tag);
     var mods = await msg.guild.members.filter(m => m.roles.has(guildConf.modRole)).map(m => m.user.tag);
+    console.log(`[${owners.length}], [${admins.length}], [${mods.length}]`)
     let embed = this.client.util.embed()
       .setTitle(`${msg.guild.name} Staff`)
       .setDescription(`This is the staff for ${msg.guild.name}. If you think you should be here, tell ${owners.join(", ")} or Samplasion#7901.`)
-      .addField("Owner" + (owners.length > 1 ? "s" : ""), owners.isE ? owners.join("\n") : "No owners.")
-      .addField("Admin" + (admins.length > 1 ? "s" : ""), admins ? admins.join("\n") : "No admins.")
-      .addField("Moderator" + (mods.length > 1 ? "s" : ""), mods ? mods.join("\n") : "No moderators.");
+      .addField("Owner" + (owners.length > 1 ? "s" : ""), !owners.isEmpty() ? owners.join("\n") : "No owners.")
+      .addField("Admin" + (admins.length > 1 ? "s" : ""), !admins.isEmpty() ? admins.join("\n") : "No admins.")
+      .addField("Moderator" + (mods.length > 1 ? "s" : ""), !mods.isEmpty() ? mods.join("\n") : "No moderators.");
     msg.embed(embed);
   }
 };
+
+Array.prototype.isEmpty = function() {
+  return this.length == 0
+}
