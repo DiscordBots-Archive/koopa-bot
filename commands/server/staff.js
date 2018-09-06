ca8efeafea013e936200155324b440ee476a902b
Glitch (koopa-bot) <none>
2018-09-06 18:48:07 +0000

    🎪🎅 Checkpoint ./commands/server/staff.js:657824/991

--- commands/server/staff.js
+++ commands/server/staff.js
@@ -14,11 +14,14 @@ module.exports = class ReplyCommand extends Command {
   run(msg) {
     const guildConf = this.client.settings.ensure(msg.guild.id, this.client.defaultSettings);
     var owners = msg.guild.members.filter(m => m.roles.has(guildConf.ownerRole)).map(m => m.user.tag);
+    var admins = msg.guild.members.filter(m => m.roles.has(guildConf.adminRole)).map(m => m.user.tag);
+    var mods = msg.guild.members.filter(m => m.roles.has(guildConf.modRole)).map(m => m.user.tag);
     let embed = this.client.util.embed()
       .setTitle(`${msg.guild.name} Staff`)
       .setDescription(`This is the staff for ${msg.guild.name}. If you think you should be here, tell ${owners.join(", ")} or Samplasion#7901.`)
-      .addField("Owner" + (owners.length > 1 ? "s" : ""))
-      .addField()
-      .addField();
+      .addField("Owner" + (owners.length > 1 ? "s" : ""), owners ? owners.join("\n") : "No owners.")
+      .addField("Admin" + (admins.length > 1 ? "s" : ""), admins ? admins.join("\n") : "No admins.")
+      .addField("Moderator" + (mods.length > 1 ? "s" : ""), mods ? mods.join("\n") : "No moderators.");
+    msg.embed(embed);
   }
 };
