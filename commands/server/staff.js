02ed9377ea956b0c0ff084e703ff865d1056e300
Glitch (koopa-bot) <none>
2018-09-06 18:58:54 +0000

    🏑⚾️ Checkpoint ./watch.json:657824/12 ./commands/server/staff.js:657824/353 ./commands/admin/warn.js:657824/118

--- commands/server/staff.js
+++ commands/server/staff.js
@@ -1,25 +1,27 @@
-const { Command } = require('classes/Command.js');
+// const { Command } = require('discord.js-commando');
+const { Command } = require('./../../classes/Command.js');
 
 module.exports = class ReplyCommand extends Command {
   constructor(client) {
     super(client, {
-      name: 'reply',
+      name: 'staff',
       group: 'server',
-      memberName: 'reply',
-      description: 'Replies with a Message.',
-      examples: ['reply']
+      memberName: 'staff',
+      description: 'Shows the staff for the current server',
+      examples: ["staff"],
+      guildOnly: true
     });
   }
 
-  run(msg) {
+  async run(msg) {
     const guildConf = this.client.settings.ensure(msg.guild.id, this.client.defaultSettings);
-    var owners = msg.guild.members.filter(m => m.roles.has(guildConf.ownerRole)).map(m => m.user.tag);
-    var admins = msg.guild.members.filter(m => m.roles.has(guildConf.adminRole)).map(m => m.user.tag);
-    var mods = msg.guild.members.filter(m => m.roles.has(guildConf.modRole)).map(m => m.user.tag);
+    var owners = await msg.guild.members.filter(m => m.roles.has(guildConf.ownerRole)).map(m => m.user.tag);
+    var admins = await msg.guild.members.filter(m => m.roles.has(guildConf.adminRole)).map(m => m.user.tag);
+    var mods = await msg.guild.members.filter(m => m.roles.has(guildConf.modRole)).map(m => m.user.tag);
     let embed = this.client.util.embed()
       .setTitle(`${msg.guild.name} Staff`)
       .setDescription(`This is the staff for ${msg.guild.name}. If you think you should be here, tell ${owners.join(", ")} or Samplasion#7901.`)
-      .addField("Owner" + (owners.length > 1 ? "s" : ""), owners ? owners.join("\n") : "No owners.")
+      .addField("Owner" + (owners.length > 1 ? "s" : ""), owners.isE ? owners.join("\n") : "No owners.")
       .addField("Admin" + (admins.length > 1 ? "s" : ""), admins ? admins.join("\n") : "No admins.")
       .addField("Moderator" + (mods.length > 1 ? "s" : ""), mods ? mods.join("\n") : "No moderators.");
     msg.embed(embed);
