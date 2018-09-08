c68cd32506587964944caa5288f0052c3a1b7426
Glitch (koopa-bot) <none>
2018-09-08 09:07:04 +0000

    👌🌌 Checkpoint ./commands/level/leaderboard.js:657824/644 ./watch.json:657824/8

--- commands/level/leaderboard.js
+++ commands/level/leaderboard.js
@@ -16,21 +16,22 @@ module.exports = class LeaderboardCommand extends Command {
                 key: "num",
                 prompt: "how many users should this leaderboard contemplate?",
                 type: "integer",
-                default: 10
+                default: 5
               }
             ]
         });
     }
 
     async run(message, { num }) {
+      /*
       // Get a filtered list (for this guild only), and convert to an array while we're at it.
-      const filtered = this.client.points.array().filter(p => p.guild === message.guild.id );
+      const filtered = this.client.points.array().filter(p => p.guild === message.guild.id);
 
       // Sort it to get the top results... well... at the top. Y'know.
       const sorted = filtered.sort((a, b) => a.points < b.points);
 
       // Slice it, dice it, get the top 10 of it!
-      const top10 = sorted.splice(0, num);
+      const top10 = sorted.splice(0, num);*/
 
       // Now shake it and show it! (as a nice embed, too!)
       const embed = this.client.util.embed()
@@ -39,9 +40,25 @@ module.exports = class LeaderboardCommand extends Command {
         .setDescription("Our top " + num + " of EXP point leaders!")
         //.setColor(0x00AE86);
       
+      // thank NYoshi for this (I don't know why it didn't work before)
+      var filtered = this.client.points.array().filter(p => p.guild === message.guild.id);
+			var sorted = filtered.sort((a, b) => b.points - a.points);
+			var top10 = sorted.splice(0, num);
+
+			console.log(top10);
+			for(const lbdata of top10) {
+				if(!message.guild.members.has(lbdata.user)) continue;
+				try {
+					embed.addField(this.client.users.get(lbdata.user).tag, `${lbdata.points} points (level ${lbdata.level})`);
+				} catch(e) {
+					console.log(e)
+				}
+			}
+      
+      /*
       for(const data of top10) {
         embed.addField(this.client.users.get(data.user).tag, `${data.points} EXP points (level ${data.level})`);
-      }
+      }*/
       
       return message.embed(embed);
     }
