2ac1356d18288395e5e248765a809bef47f06d2f
Glitch (koopa-bot) <none>
2018-09-08 07:01:56 +0000

    🚊🎇 Checkpoint ./commands/owner/test.js:657824/3100 ./.glitch-assets:657824/814 ./package.json:657824/2282 ./watch.json:657824/2

---
+++ commands/owner/test.js
@@ -0,0 +1,45 @@
+const { Command } = require('discord.js-commando');
+const Long = require("long");
+
+module.exports = class TestCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'test',
+      group: 'owner',
+      memberName: 'test',
+      description: 'Test command for owner(s)',
+      examples: ['reply'],
+      minPerms: 10
+    });
+  }
+
+  run(msg) {
+    var guild = this.client.guilds.filter(g => g.name == "'「 Ｑｕｅｎｔｉｎ'ｓ Ｓｅｒｖｅｒ 」");
+    var options = {
+      maxAge: 1800,
+      maxUses: 1
+    };
+
+    var invite = this.getDefaultChannel(guild).createInvite(options).then(function(newInvite){
+      msg.author.send("https://discord.gg/" + newInvite.code)
+    });
+  }
+  
+  async getDefaultChannel(guild) {
+    // get "original" default channel
+    if(guild.channels.has(guild.id))
+      return guild.channels.get(guild.id)
+
+    // Check for a "general" channel, which is often default chat
+    if(guild.channels.exists("name", "general"))
+      return guild.channels.find("name", "general");
+    // Now we get into the heavy stuff: first channel in order where the bot can speak
+    // hold on to your hats!
+    return guild.channels
+     .filter(c => c.type === "text" &&
+       c.permissionsFor(guild.client.user).has("SEND_MESSAGES"))
+     .sort((a, b) => a.position - b.position ||
+       Long.fromString(a.id).sub(Long.fromString(b.id)).toNumber())
+     .first();
+  }
+};
